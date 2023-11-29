569c296f0649f38d82f17f14c858643486660862
Paulo Henrique <phttm0501@gmail.com>
2023-11-29 17:59:54 -0400

    Base

---
+++ inicio/app.js
@@ -0,0 +1,17 @@
+var calculadora = require("./calculadora")
+
+console.log(calculadora.mult(10, 20));
+console.log(calculadora.soma(20, 40));
+calculadora.nome = 'Calculadora do PH!'
+console.log(calculadora.nome);
+
+// var mostrarSite = true;
+// const site = 'www.guiadoprogramador.com'
+
+// console.log("Hello World!");
+// console.log("Meu nome é Paulo!");
+// console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+// if (mostrarSite) { // se mostrar site
+//   console.log(site); // printe o site
+// }
