9a3d54a78043f21ee7ce825b38955636a7ed5a71
Paulo Henrique <phttm0501@gmail.com>
2023-12-08 13:01:41 -0400

    conditional structures with ejs

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -7,12 +7,14 @@ app.set('view engine', 'ejs')
 app.get('/:nome/:lang', (req, res) => {
   var nome = req.params.nome
   var lang = req.params.lang
+  var exibirMsg = true
   // res.render('principal/perfil') // Qunado o arquivo html se encontra em outra pasta
   res.render("index", {
     nome: nome,
     lang: lang,
     empresa: "Guia do programador",
-    inscritos: 8000
+    inscritos: 8000,
+    msg: exibirMsg
   })
 })
 
