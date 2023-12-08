82261eea7ce48bf67115593afcdd2bbdf0c7bc97
Paulo Henrique <phttm0501@gmail.com>
2023-12-07 20:18:50 -0400

    views ejs and render with express

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send("Bem vindo ao meu site!")
+  // res.render('principal/perfil') // Qunado o arquivo html se encontra em outra pasta
+  res.render("index")
 })
 
 app.listen(8080, () => { console.log("App rodando!") })
