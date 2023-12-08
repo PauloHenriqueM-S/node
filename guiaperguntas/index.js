418e5c85b4c4acd88ee06806b2d89c7034e4ab18
Paulo Henrique <phttm0501@gmail.com>
2023-12-07 20:27:09 -0400

    variables with ejs

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -4,9 +4,16 @@ const app = express()
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
+app.get('/:nome/:lang', (req, res) => {
+  var nome = req.params.nome
+  var lang = req.params.lang
   // res.render('principal/perfil') // Qunado o arquivo html se encontra em outra pasta
-  res.render("index")
+  res.render("index", {
+    nome: nome,
+    lang: lang,
+    empresa: "Guia do programador",
+    inscritos: 8000
+  })
 })
 
 app.listen(8080, () => { console.log("App rodando!") })
