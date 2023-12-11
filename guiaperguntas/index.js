e6c55e12f1e752d9348f33b24e54daac9ac2ad76
Paulo Henrique <phttm0501@gmail.com>
2023-12-11 18:30:42 -0400

    bootstrap installation

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -5,28 +5,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/:nome/:lang', (req, res) => {
-  var nome = req.params.nome
-  var lang = req.params.lang
-  var exibirMsg = false
-
-  var produtos = [
-    { nome: "Doritos", preco: 3.14 },
-    { nome: "Coca-cola", preco: 5 },
-    { nome: "Leite", preco: 1.45 },
-    { nome: "Carne", preco: 15 },
-    { nome: "Redbull", preco: 6 },
-    { nome: "Nescau", preco: 4 },
-  ]
-  // res.render('principal/perfil') // Qunado o arquivo html se encontra em outra pasta
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "Guia do programador",
-    inscritos: 8000,
-    msg: exibirMsg,
-    produtos: produtos
-  })
+app.get('/', (req, res) => {
+  res.render("index")
 })
-
 app.listen(8080, () => { console.log("App rodando!") })
