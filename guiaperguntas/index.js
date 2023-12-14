dd4ec23cefcd2d8efbf7e9c051942088019a6276
Paulo Henrique <phttm0501@gmail.com>
2023-12-13 20:22:44 -0400

    Searching for questions in the database

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -23,7 +23,12 @@ app.use(bodyParser.json())
 
 // Rotas
 app.get('/', (req, res) => {
-  res.render("index")
+  Pergunta.findAll({ raw: true }).then(perguntas => {
+    res.render("index", {
+      perguntas: perguntas
+    })
+  })
+  // select * from perguntas
 })
 
 app.get('/perguntar', (req, res) => {
