3f4f263fd0d5603c905ba6693c20c80577576a30
Paulo Henrique <phttm0501@gmail.com>
2023-12-15 21:47:43 -0400

    Displaying responses and finalizing

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -59,8 +59,17 @@ app.get('/pergunta/:id', (req, res) => {
     where: { id: id }
   }).then(pergunta => {
     if (pergunta != undefined) { // Pergunta encontrada
-      res.render('pergunta', {
-        pergunta: pergunta
+
+      Resposta.findAll({
+        where: { perguntaId: pergunta.id },
+        order: [
+          ['id', 'desc']
+        ]
+      }).then(respostas => {
+        res.render('pergunta', {
+          pergunta: pergunta,
+          respostas: respostas
+        })
       })
     } else { // Não encontrada
       res.redirect('/')
